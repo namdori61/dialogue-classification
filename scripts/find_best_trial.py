81f8b86ee2cdc71592321c13523f9ddae55e76fc
Sangho.Nam <namdori61@gmail.com>
2020-08-24 10:19:51 +0900

    Scripts: Add find best trial from optuna study.

---
+++ scripts/find_best_trial.py
@@ -0,0 +1,49 @@
+import optuna
+from absl import app, flags, logging
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('optuna_storage', default='sqlite:///optuna_studies.db',
+                    help='URI to optuna storage')
+flags.DEFINE_string('optuna_study_name', default=None,
+                    help='Study name from which find the best trial')
+
+
+def main(argv):
+    logging.info(f'Load studies from {FLAGS.optuna_storage}')
+    if not FLAGS.optuna_study_name:
+        logging.info('No optuna_study_name is given.')
+        logging.info('List of all available study names:')
+        study_summaries = (
+            optuna.get_all_study_summaries(storage=FLAGS.optuna_storage)
+        )
+        for s in study_summaries:
+            start = (
+                s.datetime_start.isoformat()
+                if s.datetime_start is not None
+                else None
+            )
+            logging.info('=' * 40)
+            logging.info(f'Name: {s.study_name}')
+            logging.info(f'Direction: {s.direction.name}')
+            logging.info(f'Trials: {s.n_trials}')
+            logging.info(f'Started at: {start}')
+        logging.info('=' * 40)
+    else:
+        logging.info(f'Find the best trial from '
+                     f'study {FLAGS.optuna_study_name}.')
+        study = optuna.load_study(study_name=FLAGS.optuna_study_name,
+                                  storage=FLAGS.optuna_storage)
+        logging.info(f'Name: {study.study_name}')
+        logging.info(f'Direction: {study.direction.name}')
+
+        best_trial = study.best_trial
+        logging.info(f'Best trial: Trial #{best_trial.number}')
+        logging.info(f'Score: {best_trial.value}')
+        logging.info('=' * 10 + ' Hyperparameters ' + '=' * 10)
+        for k, v in best_trial.params.items():
+            logging.info(f'{k}: {v}')
+
+
+if __name__ == '__main__':
+    app.run(main)
