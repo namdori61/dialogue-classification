56b4785067f25daee8a4966513370896ea96663e
Sangho.Nam <namdori61@gmail.com>
2020-10-05 16:33:26 +0900

    Model: Feat bert model.

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from models.token_model import TokenModel
 from models.jamo_cnn_model import JamoCnnModel
 from models.token_transformer_model import TokenTransformerModel
+from models.bert_model import TokenBertModel
 
-__all__ = ['TokenModel', 'JamoCnnModel', 'TokenTransformerModel']
+__all__ = ['TokenModel', 'JamoCnnModel', 'TokenTransformerModel', 'TokenBertModel']
